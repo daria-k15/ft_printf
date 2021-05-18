f54387e3a695a31a4681a8a0fdee38fc72491b46
Hsiu Eveline <heveline@oa-h1.msk.21-school.ru>
2021-05-18 21:35:53 +0300

    test

--- ft_printf.c
+++ ft_printf.c
@@ -10,35 +10,102 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-int get_spec(char *format, va_list ap)
+int	get_spec(const char *format, va_list ap)
 {
-    
+	int i;
+	i = 1;
+	if (format[i + 2] == 's')
+	{
+		while (va_arg(ap, char *) != '\0')
+			write(1, &format[i], 1);
+	}
+	else 
+		return (0);
+	return (1);
+}
+void	ft_putnbr_fd(int n)
+{
+	int	i;
+	int	nb[11];
+
+	i = 0;
+	if (n == 0)
+		write(1, "0", 1);
+	else if (n == -2147483648)
+		write(1, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write(1, "-", 1);
+			n = n * (-1);
+		}
+		while (n > 0)
+		{
+			nb[i++] = n % 10 + 48;
+			n = n / 10;
+		}
+		while (i-- > 0)
+			write(1, &nb[i], 1);
+	}
 }
 
-int ft_printf(const char *format, ...)
+
+int	ft_printf(const char *format, ...)
 {
-    va_list ap;
-
-    int i;
-
-    i = 0;
-    if (!format)
-        return (0);
-    va_start(ap, format);
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i = get_spec(&format[i + 1], ap);
-        }
-        else
-            write(1, &format[i], 1);
-        i++;
-    }
-    return (i);
+	va_list	ap;
+	int		i, j;
+	t_specif	*list;
+	list = NULL;
+	i = 0;
+	j = 0;
+	if (!format)
+		return (0);
+	va_start (ap, format);
+	char *s;
+	int d;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			i++;
+			if (format[i] == 's')
+			{
+				s = va_arg (ap, char *);
+				while (s[j] != '\0')
+				{
+					write(1, &s[j], 1);
+					j++;
+				}
+				j = 0;
+			}
+			else if (format[i] == 'c')
+			{
+				s = va_arg (ap, char *);
+				write (1, &s, 1);
+				
+			}
+			else if (format[i] == 'd')
+			{
+				d = va_arg (ap, int);
+				ft_putnbr_fd(d);
+			}
+		}
+		else
+			write(1, &format[i], 1);
+		i++;
+	}
+	va_end(ap);
+	return (i);
 }
 
-int main ()
+int	main(void)
 {
-    ft_printf("qwerty %", 2);
+	ft_printf("%%%  %s %c %%%%  %s %s %d", "qwerty",'g', "hey", "there", 1458796);
+	printf("\n");
+	ft_printf("%c", 'p');
+	printf("\n");
+	ft_printf("%d", 45789685);
+	
+	//printf("%c", 'cfd');
 }
--- printf.h
+++ printf.h
@@ -1,19 +1,18 @@
 #ifndef PRINTF_H
 # define PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
 
-typedef struct  s_specif
+typedef struct s_specif
 {
-    int     flag_minus;
-    int     flag_zero;
-    int     flag_dot;
-    int     prec;
-    int     length;
-    char    type;
-}               t_specif;
-
+	int		flag_minus;
+	int		flag_zero;
+	int		flag_dot;
+	int		prec;
+	int		length;
+	char	type;
+}				t_specif;
 
 #endif
